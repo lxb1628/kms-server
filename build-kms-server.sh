ca3024a34d4498b476b796265c300d759ecd8292
jts.sh <yygfml@163.com>
2018-04-15 14:49:40 +0800

    Update build-kms-server.sh

--- build-kms-server.sh
+++ build-kms-server.sh
@@ -6,16 +6,12 @@ set -e
 mkdir /tmp/docker-kms/ && cd /tmp/docker-kms/
 if [ ! -e Dockerfile ]; then 
   cat >Dockerfile <<-'EOF'
-                  #第一行必须指令基于的基础镜像
                   FROM centos
-                  #镜像的操作指令
                   RUN cd /tmp/ && curl -L https://github.com/Wind4/vlmcsd/releases/download/svn1111/binaries.tar.gz | tar -zx
                   RUN mv /tmp/binaries/Linux/intel/static/vlmcsdmulti-x64-musl-static /usr/local/bin/vlmcsdmulti-x64-musl-static
                   RUN cd /usr/local/bin/ && chmod +x vlmcsdmulti-x64-musl-static
                   RUN rm -rf /tmp/binaries
-                  #容器启动时执行指令
                   CMD vlmcsdmulti-x64-musl-static vlmcsd -L 0.0.0.0:1688 -e -D
-                  #容器暴露端口
                   EXPOSE 1688
    EOF
 fi
