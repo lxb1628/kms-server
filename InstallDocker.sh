79bf39ec04743d16761118cfdd19137512acc564
jts.sh <yygfml@163.com>
2018-04-15 14:48:46 +0800

    Update InstallDocker.sh

--- InstallDocker.sh
+++ InstallDocker.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
-
+set -e
 #description: Install Docker script by lxb1628 <lxb1628@163.com>
 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 clear
 do_Install(){
-	yum install -y yum-utils device-mapper-persistent-data lvm2 curl
 	yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-	yum install -y docker-ce
+	yum install -y yum-utils device-mapper-persistent-data lvm2 curl docker-ce
 	systemctl enable docker
 	systemctl start docker
 	curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
