36b25d95b56365ec66c795e1660370505bd9da5f
李小兵 <yygfml@live.cn>
2018-11-20 02:12:07 +0800

    update

--- InstallDocker.sh
+++ InstallDocker.sh
@@ -6,12 +6,12 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 clear
 do_Install(){
-        yum install -y yum-utils
+    yum install -y yum-utils
 	yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 	yum install -y device-mapper-persistent-data lvm2 curl docker-ce
 	systemctl enable docker
 	systemctl start docker
-	curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+	curl -L https://github.com/docker/compose/releases/download/1.23.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 	chmod +x /usr/local/bin/docker-compose
 	clear
 	echo "Succeeded."
