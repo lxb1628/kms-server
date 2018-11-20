e15350e8ea723327c74da17ee14aa83fa45061e5
kms-server <yygfml@163.com>
2018-11-20 10:52:40 +0800

    Update build-kms-server-a.sh

--- build-kms-server-a.sh
+++ build-kms-server-a.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-#description: Build Docker Container script by lxb1628 <lxb1628@163.com>
+#description: Build Docker Container script by yygfml <yygfml@163.com>
 
 # git clone vlmcsd
 cd /tmp/ && git clone https://github.com/Wind4/vlmcsd.git
@@ -10,13 +10,26 @@ cd bin && mv vlmcsd /tmp/docker-kms
 cd /tmp/docker-kms && rm -rf /tmp/vlmcsd
 
 # create Dockerfile Script
-if [ ! -e Dockerfile ]; then 
-  cat >Dockerfile <<-'EOF'
+if [ ! -e Dockerfile ]; then
+tee >Dockerfile << 'EOF'
   FROM alpine:latest
-  COPY vlmcsd /usr/local/bin/
+  MAINTAINER yygfml yygfml@163.com
+
+  ******************更换Alpine源为阿里云******************
+  RUN echo "https://mirrors.aliyun.com/alpine/v3.8/main" /etc/apk/repositories && \
+	  echo "https://mirrors.aliyun.com/alpine/v3.8/community" >> /etc/apk/repositories
+  RUN apk update && apk upgrade
+  ********Alpine安装 Glibc https://github.com/sgerrand/alpine-pkg-glibc **********
+  RUN apk --no-cache add ca-certificates && \
+      wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://raw.githubusercontent.com/sgerrand/alpine-pkg-glibc/master/sgerrand.rsa.pub && \
+	  wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.27-r0/glibc-2.27-r0.apk && \
+	  apk add glibc-2.28-r0.apk
+
+  WORKDIR /vlmcsd
+  ONBUILD COPY vlmcsd /vlmcsd/
   EXPOSE 1688
-  CMD /usr/local/bin/vlmcsd -L 0.0.0.0:1688 -e -D
-EOF
+  CMD /vlmcsd/vlmcsd -L 0.0.0.0:1688 -e -D
+  EOF
 fi
 
 # build kms-server:latest container
