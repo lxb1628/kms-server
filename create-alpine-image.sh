d810e0c2b606f443ddce558f26eb14e994f98454
kms-server <yygfml@163.com>
2018-11-22 01:27:22 +0800

    Update create-alpine-image.sh

--- create-alpine-image.sh
+++ create-alpine-image.sh
@@ -17,7 +17,7 @@ if [ ! -e Dockerfile ]; then
   FROM alpine:latest
   
   # MAINTAINER
-  MAINTAINER yygfml yygfml@163.com
+  MAINTAINER yygfml<yygfml@163.com>
   # put vlmcsd into /usr/local/bin
   ADD vlmcsd /usr/local/bin
   
