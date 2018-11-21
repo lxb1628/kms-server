996fae475cd72caf43f8b5daf289c316e3308ccc
kms-server <yygfml@163.com>
2018-11-21 14:00:34 +0800

    Update create-apline-image.sh

--- create-apline-image.sh
+++ create-apline-image.sh
@@ -4,16 +4,16 @@ set -e
 #description: Build Docker Container script by lxb1628 <lxb1628@163.com>
 
 # git clone vlmcsd
-cd /tmp/ && wget 
-cd vlmcsd && make
-mkdir /tmp/docker-kms
-cd bin && mv vlmcsd /tmp/docker-kms
-cd /tmp/docker-kms && rm -rf /tmp/vlmcsd
+cd /tmp/ && wget https://github.com/Wind4/vlmcsd/releases/download/svn1112/binaries.tar.gz
+tar -xzvf binaries.tar.gz
+
+cd /tmp/binaries/Linux/intel/musl && mv vlmcsdmulti-x64-musl /tmp/docker-kms/vlmcsd
+cd /tmp/docker-kms && rm -rf /tmp/vlmcsd/binaries
 
 # create Dockerfile Script
 if [ ! -e Dockerfile ]; then 
-  cat >Dockerfile <<-'EOF'
-  FROM centos:latest
+  tee >Dockerfile <<EOF
+  FROM alpine:latest
   ADD vlmcsd /usr/local/bin/
   EXPOSE 1688
   CMD vlmcsd -L 0.0.0.0:1688 -e -D
