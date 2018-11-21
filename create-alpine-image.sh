b5c61b9267b54d772c6e5cf4960ddda7a3cbb870
kms-server <yygfml@163.com>
2018-11-22 01:08:52 +0800

    Update create-alpine-image.sh

--- create-alpine-image.sh
+++ create-alpine-image.sh
@@ -13,16 +13,9 @@ cd /tmp/docker-kms && rm -rf /tmp/binaries*
 # create Dockerfile Script
 if [ ! -e Dockerfile ]; then 
   tee >Dockerfile <<-'EOF'
-  # base image
   FROM alpine:latest
-  
-  # MAINTAINER
   MAINTAINER yygfml yygfml@163.com
-  
-  # put kms file
   ADD vlmcsd /usr/local/bin/
-  
-  # execute command to compile kms
   CMD vlmcsd -L 0.0.0.0:1688 -e -D
   EXPOSE 1688
 EOF
