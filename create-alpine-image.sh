80c0c6dc93d926b3ce8e18594e83bbbe5b5142a0
kms-server <yygfml@163.com>
2018-12-19 08:56:37 +0800

    Update create-alpine-image.sh

--- create-alpine-image.sh
+++ create-alpine-image.sh
@@ -2,14 +2,25 @@
 #!/bin/sh
 set -e
 #description: Build Docker Container script by lxb1628 <lxb1628@163.com>
+#
+#Some Scripts Copyed by Wind4
+#
 
-# git clone vlmcsd
-cd /tmp/ && wget https://github.com/Wind4/vlmcsd/releases/download/svn1112/binaries.tar.gz
+TMP_DIR=`mktemp -d`
+GIT_TAG=svn1112
+cd ${TMP_DIR}
+
+echo 'Downloading vlmcsd ...'
+wget -q https://github.com/Wind4/vlmcsd/releases/download/${GIT_TAG}/binaries.tar.gz -O binaries.tar.gz
+check_result $? 'Download vlmcsd failed.'
+
+echo 'Extract vlmcsd ...'
 tar -xzvf binaries.tar.gz
 mkdir /tmp/docker-kms
-cd /tmp/binaries/Linux/intel/musl && mv vlmcsdmulti-x64-musl /tmp/docker-kms/vlmcsd
-cd /tmp/docker-kms && rm -rf /tmp/binaries*
+cp binaries/Linux/intel/musl/vlmcsdmulti-x64-musl /tmp/docker-kms/vlmcsd
 
+echo 'Create Docker Image ...'
+cd /tmp/docker-kms
 # create Dockerfile Script
 if [ ! -e Dockerfile ]; then 
   cat >Dockerfile <<-'EOF'
@@ -34,5 +45,9 @@ docker build -t kms-server:latest .
 #docker run -d kms-server:latest
 docker run -d -p 1688:1688 --restart=always --name kms kms-server
 
-#clear tmp file
-cd ~ && rm -rf /tmp/docker-kms
+echo 'Cleaning ...'
+cd ~
+rm -rf ${TMP_DIR}
+rm -rf /tmp/docker-kms
+
+echo 'Installed successfully.'
